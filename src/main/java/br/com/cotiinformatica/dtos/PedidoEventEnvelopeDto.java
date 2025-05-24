f3e90897981dbdf7891416cce9eac7f5b98f5004
KaioMuniz <kkaioribeiro@gmail.com>
2025-05-24 11:45:50 -0300

    PedidoEventEnvelopeDto

---
+++ src/main/java/br/com/cotiinformatica/dtos/PedidoEventEnvelopeDto.java
@@ -0,0 +1,44 @@
+package br.com.cotiinformatica.dtos;
+
+import java.util.Date;
+import java.util.UUID;
+
+import br.com.cotiinformatica.entities.Pedido;
+
+public class PedidoEventEnvelopeDto {
+
+	private UUID id;
+	private Date dataGeracao;
+	private Pedido pedido;
+	
+	public PedidoEventEnvelopeDto() {
+		super();
+	}
+	
+	public PedidoEventEnvelopeDto(UUID id, Date dataGeracao, Pedido pedido) {
+		super();
+		this.id = id;
+		this.dataGeracao = dataGeracao;
+		this.pedido = pedido;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public Date getDataGeracao() {
+		return dataGeracao;
+	}
+	public void setDataGeracao(Date dataGeracao) {
+		this.dataGeracao = dataGeracao;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+}
